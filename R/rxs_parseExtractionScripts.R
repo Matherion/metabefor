4421be27774fe7257fa8c5bf428cbaf6b412e49e
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2018-07-09 10:39:12 +0200

    Corrected file in purl command

--- R/rxs_parseExtractionScripts.R
+++ R/rxs_parseExtractionScripts.R
@@ -18,7 +18,7 @@ rxs_parseExtractionScripts <- function(path,
     ### From https://stackoverflow.com/questions/24753969/knitr-run-all-chunks-in-an-rmarkdown-document
     tempR <- tempfile(tmpdir = ".", fileext = ".R");
     on.exit(unlink(tempR));
-    knitr::purl(rmd, output=tempR);
+    knitr::purl(file.path(path, filename), output=tempR);
     res$rxsOutput[[filename]] <-
       capture.output(tryCatch(sys.source(tempR, envir=globalenv())),
                              error = function(e) {
